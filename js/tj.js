922fdf94e18c19a4ffc6d399a9f24305de961170
kk-a <chq8@foxmail.com>
2018-05-15 02:43:25 -0500

    Create tj.js

---
+++ js/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?49fbf570b595e2122e7d0b679ea74e80";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
